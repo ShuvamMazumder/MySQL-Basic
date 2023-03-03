6f65c596b4de4c41b4bfe136f6a64806da56a2c0
Shuvam Mazumder <shuvam.baiust@gmail.com>
2023-03-03 16:11:36 +0600

    Add files via upload

--- MySQL from ZERO.sql
+++ MySQL from ZERO.sql
@@ -28,11 +28,50 @@ alter table employees
 modify email varchar(100)
 after last_name;
 
+alter table employees
+drop column email;
 
+insert into employees
+values (1,'shuvam','mazumder',5,'2022-5-5');
 
+insert into employees 
+values  (2,'tom','grey',5.6,'2020-6-6'),
+		(3,'janina','grey',6,'2020-6-6'),
+        (4,'asdlkf','grey',6,'2020-6-6'),
+        (5,'sandy','grey',5,'2020-6-6');
 
+-- missing data insert
+insert into employees (employee_id,first_name,last_name)
+values (6,'sheldon','han');
 
+update employees
+set hourly_pay = 10
+where employee_id = 6;
 
+set autocommit = off;
+
+create table test (
+	my_date date,
+    my_time time,
+    my_datetime datetime
+);
+select * from test;
+
+insert into test
+values (current_date(),current_time(),now());
+
+create table products(
+	product_id int,
+    product_name varchar(25),
+	price decimal(4,2)
+);
+
+select * from products;
+
+insert into products 
+values (100,'hamburger',3.99),
+		(101,'fries',19),
+		(102,'cola',25);
 
 
 
